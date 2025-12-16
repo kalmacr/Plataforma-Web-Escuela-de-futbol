4e95d39e901c753aace24904e815ba801a560027
Kevin Calvo <166265026+kalmacr@users.noreply.github.com>
2025-12-16 07:47:29 -0600

    Primer commit: agrego proyecto CubunjuquiFC ASP.NET 8

---
+++ CubunjuquiFC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CubunjuquiFC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
